ee0ddeff9bff74ba32d1f0501518c0cecc378786
Manuel Dithurbide <mdithurbide@itba.edu.ar>
2022-05-20 20:01:47 -0300

    Comienzo de la libreria en Userland

---
+++ x64barebones/Userland/Lib/stdlib.c
@@ -0,0 +1,45 @@
+#include "stdlib.h"
+
+void num_to_string(long num, char * string){
+    int i = 0;
+    while(num > 0){
+        string[i++] = num%10 + '0';
+        num /= 10;
+    }
+    string[i] = 0;
+    reverseString(string);
+}
+
+void reverseString(char * string){
+    int i, j;
+    char aux;
+    j = strlen(string)-1;
+    for(i = 0 ; i<j ; i++, j--){
+        aux = string[i];
+        string[i] = string[j];
+        string[j] = aux;
+    }
+}
+
+int strContains(char * string1, char * string2){
+    int i, j;
+    int contains = 0;
+    for(i = 0 ; i < strlen(string1) && !contains ; i++){
+        for(j = 0 ; j < strlen(string2) ; j++){
+            if(string1[i] == string2[j])
+                contains = 1;
+            else   
+                contains = 0;
+        }
+    }
+    return contains;
+}
+
+int strlen(char * string){
+    int i = 0;
+    while(string[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
---
+++ x64barebones/Userland/Lib/stdlib.h
@@ -0,0 +1,4 @@
+void num_to_string(long num, char * string);
+void reverseString(char * string);
+int strContains(char * string1, char * string2);
+int strlen(char * string);
