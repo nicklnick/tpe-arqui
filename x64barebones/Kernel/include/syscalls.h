f491cf2dea8c496d14df77432c07e95765793e84
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-05-22 11:34:55 -0300

    Added sys_clear_screen() to syscalls.h

--- x64barebones/Kernel/include/syscalls.h
+++ x64barebones/Kernel/include/syscalls.h
@@ -5,4 +5,15 @@
 unsigned int sys_write(unsigned int fd, const char *buf, unsigned int count);
 unsigned int sys_read(unsigned int fd, char * buf, unsigned int count);
 
+/*
+ * << sys_clear_screen >>
+ * ----------------------------------------------------------------------
+ * Description: Clears STODUT
+ * ----------------------------------------------------------------------
+ * Receives: --
+ * Returns: 
+ *      0 if successful 
+ */
+unsigned int sys_clear_screen();
+
 #endif
